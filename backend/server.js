ed52f46dffae6d16479b92f803b79b27cf41dad3
Madhulatha17 <bandimadhulatha4@gmail.com>
2026-06-21 21:57:50 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/weather", require("./routes/weatherRoutes"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
